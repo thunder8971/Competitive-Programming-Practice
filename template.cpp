50d6f3dd48b4364dbf52e13fd1bc654c151cd7bd
thunder8971 <ricardoparedes0922@gmail.com>
2026-07-09 19:10:30 -0500

    template :3

---
+++ template.cpp
@@ -0,0 +1,38 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define forsn(i, s, n) for (int i = int(s); i < int(n); i++)
+#define dforn(i, n) for (int i = int(n) - 1; i >= 0; i--)
+#define dforsn(i, s, n) for (int i = int(n) - 1; i >= int(s); i--)
+#define all(x) x.begin(), x.end()
+#define mk make_pair
+#define ff first
+#define ss second
+#define ln '\n'
+template <typename T>
+int sz(const T &x) {
+    return int(x.size());
+}
+
+void go() {
+
+
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tt = 1;
+    // cin >> tt;
+    
+    forn (tc, tt) {
+        go();
+    }
+
+    return 0;
+}
